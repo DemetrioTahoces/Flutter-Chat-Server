3ca70d970406ea017e9dda220da56edd6c2f2612
tonitahoces <tonitahoces@gmail.com>
2021-01-25 19:37:40 +0100

    users

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,17 @@
 const { request, response } = require("express");
+const User = require('../models/user');
 
 const getUsers = (req = request, res = response) => {
 
+    const users = await User.find({
+        _id: {
+            $ne: req.uuid,
+        },
+    }).sort('-online');
+
     return res.json({
         ok: true,
-        msg: 'aaaaaaaa',
+        users,
     })
 }
 
