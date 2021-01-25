4c0d55dfffa37e878e0170afb62421bae5a8d91a
tonitahoces <tonitahoces@gmail.com>
2021-01-25 19:22:34 +0100

    online

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,7 +3,7 @@ const { io } = require('../index');
 const { userConnected, userDisconnected } = require('../controllers/socket');
 
 // Mensajes de sockets
-io.on('connection', async client => {
+io.on('connection', client => {
     const token = client.handshake.headers['authorization'];
     const uuid = validateJWT(token);
 
@@ -13,9 +13,9 @@ io.on('connection', async client => {
     }
 
     // Client authenticated
-    await userConnected(uuid);
+    userConnected(uuid);
 
     client.on('disconnect', () => {
-        await userDisconnected(uuid);
+        userDisconnected(uuid);
     });
 });
