b4aab2950d71e093a39856c794dc32a4b252cdd9
tonitahoces <tonitahoces@gmail.com>
2021-01-25 19:41:21 +0100

    users

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,16 @@ const User = require('../models/user');
 
 const getUsers = async(req = request, res = response) => {
 
-
+    const from = Number(req.query.from) || 0;
+    const limit = Number(req.query.limit) || 20;
 
     const users = await User.find({
-        _id: {
-            $ne: req.uuid,
-        },
-    }).sort('-online');
+            _id: {
+                $ne: req.uuid,
+            },
+        }).sort('-online')
+        .skip(from)
+        .limit(limit);
 
     return res.json({
         ok: true,
