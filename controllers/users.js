05bf1108809d53b7d17ac397c5c5c335e0cc76e4
tonitahoces <tonitahoces@gmail.com>
2021-01-25 19:29:41 +0100

    users

---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+const { request, response } = require("express");
+
+const getUsers = (req = request, res = response) => {
+
+    return res.json({
+        ok: true,
+        msg: 'aaaaaaaa',
+    })
+}
